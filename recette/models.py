5e2f5a2459361560a631509e752532caaa639110
Gen709 <709ruebeaudoin@gmail.com>
2022-07-16 17:11:52 -0400

    ajout du model recette

---
+++ recette/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from canadiannutrientfile.models import FoodName
+# ConversionFactor, NutrientAmount
+from debugpy.common.json import default
+# Create your models here.
