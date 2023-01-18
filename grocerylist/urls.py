662f9f2a71be76234a0088f1c7914175e871e0ff
Gen709 <709ruebeaudoin@gmail.com>
2023-01-18 12:33:13 -0500

    test

--- grocerylist/urls.py
+++ grocerylist/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('wishlist/', grocery_list_view, name='wishlist'),
     path('wishlist/update/', grocery_store_list_update_view, name='wishlist-update'),
-    # path('wishlist/pay/', grocery_store_list_pay_view, name='wishlist-pay'),
+    path('wishlist/pay/', grocery_store_list_pay_view, name='wishlist-pay'),
     path('store/create/', grocery_store_create_view, name='grocery-store-create'),
     path('store/list/', grocery_store_list_view, name='grocery-store-list'),
     
